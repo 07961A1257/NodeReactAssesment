d1396ef5658619b39221e53a705fddd893f3dcd7
07961A1257 <yvakchaitanya@gmail.com>
2022-07-06 12:04:55 +0530

    Created Inserted Scripts

---
+++ backend/INSERT_USER.sql
@@ -0,0 +1,11 @@
+INSERT INTO sys.`user` (name, email, password, `role`) 
+VALUES('James', 'James@123.com', '1!23#4', 'EMPLOYEE');
+
+INSERT INTO sys.`user` (name, email, password, `role`) 
+VALUES('Peter', 'Peter@123.com', '8^23!3', 'EMPLOYEE');
+
+INSERT INTO sys.`user` (name, email, password, `role`) 
+VALUES('John', 'John@123.com', '98!89!', 'ADMIN');
+
+INSERT INTO sys.`user` (name, email, password, `role`) 
+VALUES('Fred', 'Fred@123.com', '68651', 'ADMIN');
